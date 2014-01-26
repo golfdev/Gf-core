77112f5c294efddb8983c5cf13c370e49c24e3e6
zenglvlin <lvlinzeng@gmail.com>
2014-01-26 15:08:50 +0800

    xiugai

--- src/com/jinfang/golf/appointment/home/GolfAppointmentHome.java
+++ src/com/jinfang/golf/appointment/home/GolfAppointmentHome.java
@@ -3,6 +3,7 @@ package com.jinfang.golf.appointment.home;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import com.jinfang.golf.appointment.model.GolfAppointment;
 import com.jinfang.golf.appointment.model.GolfAppointmentMember;
 import com.jinfang.golf.club.home.GolfClubHome;
 import com.jinfang.golf.club.model.GolfClub;
+import com.jinfang.golf.constants.GolfConstant;
 import com.jinfang.golf.team.home.UserTeamHome;
 import com.jinfang.golf.user.home.UserHome;
 import com.jinfang.golf.user.model.User;
@@ -55,10 +57,25 @@ public class GolfAppointmentHome {
 				List<Integer> uidList = golfAppointmentMemberDAO
 						.getUserIdListByAppointId(appoint.getId());
 				List<User> userList = userHome.getUserListByIds(uidList);
+				for(User user:userList){
+					if (StringUtils.isNotBlank(user.getHeadUrl())) {
+						user.setHeadUrl(GolfConstant.IMAGE_DOMAIN
+								+ user.getHeadUrl());
+					} else {
+						user.setHeadUrl(GolfConstant.IMAGE_DOMAIN
+								+ GolfConstant.DEFAULT_HEAD_URL);
+					}
+				}
 				appoint.setUserList(userList);
 				GolfClub club = golfClubHome.getGolfClubById(appoint.getClubId());
 				appoint.setClubName(club.getName());
-				appoint.setClubLogo(club.getLogo());
+				if (StringUtils.isBlank(club.getLogo())) {
+					appoint.setClubLogo(GolfConstant.IMAGE_DOMAIN
+							+ GolfConstant.DEFAULT_CLUB_LOGO_URL);
+				} else {
+					appoint.setClubLogo(GolfConstant.IMAGE_DOMAIN
+							+ club.getLogo());
+				}
 			}
 			return appointList;
 		} else {
@@ -68,10 +85,25 @@ public class GolfAppointmentHome {
 				List<Integer> uidList = golfAppointmentMemberDAO
 						.getUserIdListByAppointId(appoint.getId());
 				List<User> userList = userHome.getUserListByIds(uidList);
+				for(User user:userList){
+					if (StringUtils.isNotBlank(user.getHeadUrl())) {
+						user.setHeadUrl(GolfConstant.IMAGE_DOMAIN
+								+ user.getHeadUrl());
+					} else {
+						user.setHeadUrl(GolfConstant.IMAGE_DOMAIN
+								+ GolfConstant.DEFAULT_HEAD_URL);
+					}
+				}
 				appoint.setUserList(userList);
 				GolfClub club = golfClubHome.getGolfClubById(appoint.getClubId());
 				appoint.setClubName(club.getName());
-				appoint.setClubLogo(club.getLogo());
+				if (StringUtils.isBlank(club.getLogo())) {
+					appoint.setClubLogo(GolfConstant.IMAGE_DOMAIN
+							+ GolfConstant.DEFAULT_CLUB_LOGO_URL);
+				} else {
+					appoint.setClubLogo(GolfConstant.IMAGE_DOMAIN
+							+ club.getLogo());
+				}
 			}
 			return appointList;
 
--- src/com/jinfang/golf/course/dao/GolfCourseCommentDAO.java
+++ src/com/jinfang/golf/course/dao/GolfCourseCommentDAO.java
@@ -13,9 +13,9 @@ public interface GolfCourseCommentDAO {
 	//数据表名
 	public static String table_name = "golf_course_comment";
 	//数据表字段
-	public static String field = "id,course_id,content,type,created_time";
+	public static String field = "id,user_id,course_id,content,type,created_time";
 
-	@SQL(" insert into " + table_name + "(course_id,content,type,created_time)values(:1.courseId,:1.content,:1.type,now())")
+	@SQL(" insert into " + table_name + "(course_id,user_id,content,type,created_time)values(:1.courseId,:1.userId,:1.content,:1.type,now())")
 	public Identity save(GolfCourseComment comment);
 	
 	@SQL(" select "+field+" from " + table_name + " where course_id=:1 order by created_time desc limit :2,:3")
